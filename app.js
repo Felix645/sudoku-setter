7b4515c582bc834e0b1d9b02c67797784fb06949
Kuntscher <f.kuntscher@palmer.ag>
2022-01-03 16:31:34 +0100

    Added initial files

---
+++ app.js
@@ -0,0 +1,23 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8 ,9];
+
+const grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+let checkNumber = function(grid, number, row, column) {
+    let checkRow = function(grid, number, row, column) {
+        let row = grid[row];
+
+        row.forEach(function(item, index) {
+            
+        });
+    }
+};
